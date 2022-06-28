b7176c48b9afd5cf3a19170ccbced9cce90b94b2
Miha <rizoks29@gmail.com>
2022-06-28 21:28:39 +0300

    Changes

---
+++ client_packages/src/events/rentvehicles/events.ts
@@ -0,0 +1,14 @@
+export let rentVehicleInfo: string
+let vehicleRentMenue: BrowserMp
+
+mp.events.add("activateVehicleRentMenue",(activate: boolean,text: string,vehicleName: string) => {
+    if (activate){
+        mp.gui.cursor.show(true,true)
+        rentVehicleInfo = text
+        vehicleRentMenue = mp.browsers.new("package://cef/rentvehicles/index.html")
+    } else {
+        mp.game.ui.setNotificationTextEntry(`Ви орендували: ~g~${vehicleName}`)
+        mp.gui.cursor.show(false,false)
+        vehicleRentMenue.destroy()
+    }
+})
