049918f44713ec3623105ccd62b12e8dc0f1b367
Miha <rizoks29@gmail.com>
2022-06-28 21:23:48 +0300

    Changes

---
+++ client_packages/src/events/peds/controls.ts
@@ -0,0 +1,8 @@
+import dialogPeds from "./main"
+
+mp.events.add(RageEnums.EventKey.RENDER, () => {
+    const dialogPed = dialogPeds.find(dialogPed => dialogPed.data.dialogActive)
+    if (dialogPed){
+        mp.game.controls.disableAllControlActions(0) 
+    }
+})
