d25c823c96c421cc998bcafd2cc37bbbf8bda19a
Miha <rizoks29@gmail.com>
2022-06-29 23:12:42 +0300

    Changes

--- client_packages/src/globals/vehicles/engine.ts
+++ client_packages/src/globals/vehicles/engine.ts
@@ -1,9 +1,9 @@
+mp.game.vehicle.defaultEngineBehaviour = false
+
 mp.events.add('playerEnterVehicle', () => { 
-    mp.game.vehicle.defaultEngineBehaviour = false
     mp.players.local.setConfigFlag(429, true) //PED_FLAG_STOP_ENGINE_TURNING
 })
 
 mp.events.add('playerLeaveVehicle', () => {
-    mp.game.vehicle.defaultEngineBehaviour = false
     mp.players.local.setConfigFlag(241,true) //PED_FLAG_DISABLE_STOPPING_VEH_ENGINE
 })
