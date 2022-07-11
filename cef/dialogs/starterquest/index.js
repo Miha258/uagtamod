6d688b2248b7a1fa59a2a69308a199aebfe793c6
Miha <rizoks29@gmail.com>
2022-07-11 15:45:20 +0300

    Changes

---
+++ cef/dialogs/starterquest/index.js
@@ -0,0 +1,67 @@
+const App = {
+    data() {
+        return {
+            dialogPhrases: '',
+            phraseSetter: '',
+            phraseGen: this.getPhrase(),
+            phrase: '',
+            buttons: [],
+        }
+    },
+    methods: {
+        async getButtons(){
+            const dialogButtons = await mp.events.callProc("reciveStarterButtons")
+            if (dialogButtons){
+                return dialogButtons.split(' ')
+            }
+        },
+        async getPhrases(){
+            const dialogPhrases = await mp.events.callProc("reciveStarterDialog")
+            return dialogPhrases.split('  ')
+        },
+        async * getPhrase(){
+            for (phrase of await this.getPhrases()){
+                yield phrase
+            }
+        },
+        async setPhrase() {
+            let content = await this.phraseGen.next()
+            if (content.done){ 
+                const dialogButtons = await this.getButtons()
+                if (dialogButtons){
+                    this.buttons = dialogButtons
+                    this.phrase = 1
+                    clearInterval(this.phraseSetter)
+                    mp.trigger("showCursor", true)
+                } else {
+                    mp.trigger('closeStarterDialog')
+                }
+            }
+            this.phrase = await content.value
+        },
+        clickCallbackButton(event){
+            mp.trigger("showCursor", false)
+            mp.trigger("buttonCallback:"+event.target.innerText)
+            mp.trigger("closeStarterDialog")
+        }
+    },   
+    async mounted() {
+        const phrase = await this.phraseGen.next()
+        this.phrase = phrase.value
+        this.phraseSetter = setInterval(async () => {
+            await this.setPhrase()
+        },5000)
+    },
+}
+
+Vue.createApp(App).mount('#app')
+
+
+
+
+
+
+
+
+
+
