f2df450884e4316f90b0f444c2442b66ff6a40da
Miha258 <77239046+Miha258@users.noreply.github.com>
2022-07-11 23:43:44 +0300

    Add files via upload

---
+++ client_packages/src/hud/alerts.ts
@@ -0,0 +1,19 @@
+import { hud } from './main'
+
+
+mp.events.add('sendInfoAlert', (info: string) => {
+    hud.execute(`window.trigger(\'sendInfoAlert\', ${info})`)
+})
+
+mp.events.add('sendDoneAlert', (done: string) => {
+    hud.execute(`window.trigger(\'sendDoneAlert\', ${done})`)
+})
+
+mp.events.add('sendWarningAlert', (waring: string) => {
+    hud.execute(`window.trigger(\'sendWarningAlert\', ${waring})`)
+})
+
+mp.events.add('sendErrorAlert', (error: string) => {
+    hud.execute(`window.trigger(\'sendErrorAlert\', ${error})`)
+})
+
